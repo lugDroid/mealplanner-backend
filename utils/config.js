99738cd3456c6d2bdd470398f3456e476c82a029
lugDroid <diazbastos@gmail.com>
2023-02-10 15:08:26 +0100

    Add development db connection

--- utils/config.js
+++ utils/config.js
@@ -1,9 +1,23 @@
+/* eslint-disable indent */
 require("dotenv").config();
 
-const MONGODB_URI =
+let MONGODB_URI;
+
+switch (process.env.NODE_ENV) {
+  case "test":
+    MONGODB_URI = process.env.TEST_MONGODB_URI;
+    break;
+  case "development":
+    MONGODB_URI = process.env.DEV_MONGODB_URI;
+    break;
+  default:
+    MONGODB_URI = process.env.MONGODB_URI;
+}
+
+/* const MONGODB_URI =
   process.env.NODE_ENV === "test"
     ? process.env.TEST_MONGODB_URI
-    : process.env.MONGODB_URI;
+    : process.env.MONGODB_URI; */
 
 const PORT = process.env.PORT;
 
