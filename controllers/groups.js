3848fcc56859b1729564aebbf2ebd77296c2c789
lugDroid <diazbastos@gmail.com>
2023-03-14 15:43:39 +0100

    Fix access problems for Group controller methods

--- controllers/groups.js
+++ controllers/groups.js
@@ -8,8 +8,13 @@ groupsRouter.get("/", async (req, res) => {
     return res.status(401).json({ error: "invalid token" });
   }
 
-  const groups = await Group.find({}).populate("user", { username: 1, name: 1 });
-  res.json(groups);
+  const groups = await Group.find({ user: req.decodedToken.id }).populate("user", { username: 1, name: 1 });
+
+  if (groups.length !== 0) {
+    res.json(groups);
+  } else {
+    res.status(204).end();
+  }
 });
 
 groupsRouter.get("/:id", async (req, res) => {
@@ -23,7 +28,9 @@ groupsRouter.get("/:id", async (req, res) => {
 
   const group = await Group.findById(req.params.id).populate("user", { username: 1, name: 1 });
 
-  if (group) {
+  if (group.user.id !== req.decodedToken.id) {
+    res.status(403).end();
+  } else if (group) {
     res.json(group);
   } else {
     res.status(404).end();
@@ -36,10 +43,16 @@ groupsRouter.delete("/:id", async (req, res) => {
   }
 
   const user = await User.findById(req.decodedToken.id);
+  const removedGroup = await Group.findById(req.params.id);
 
-  const removedGroup = await Group.findByIdAndRemove(req.params.id);
+  if (removedGroup && removedGroup.user.toString() !== req.decodedToken.id) {
+    res.status(403).end();
+    return;
+  }
 
   if (removedGroup) {
+    await Group.deleteOne({ _id: req.params.id });
+
     // we also need to remove the group from the user document
     user.groups = user.groups.filter(groupId => groupId.toString() !== removedGroup.id);
     await user.save();
@@ -78,6 +91,12 @@ groupsRouter.put("/:id", async (req, res) => {
 
   if (!req.decodedToken) {
     return res.status(401).json({ error: "invalid token" });
+  }
+
+  const group = await Group.findById(req.params.id);
+  if (group && group.user.toString() !== req.decodedToken.id) {
+    res.status(403).end();
+    return;
   }
 
   const user = await User.findById(req.decodedToken.id);
